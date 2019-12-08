0bb7559097b305288343a8575b169ff306a0516e
Kevin Zhang <hello@kevinzha.ng>
2019-12-08 03:39:58 -0500

    optimize js

--- gulpfile.js
+++ gulpfile.js
@@ -35,12 +35,18 @@ function css(cb) {
 function js(cb) {
   gulp
     .src([
-      "./assets/js/jquery-1.11.3.min.js",
       "./assets/js/jquery.history.js",
       "./assets/js/jquery.fitvids.js",
-      "./assets/ghostHunter/dist/jquery.ghosthunter.js",
       "./assets/js/nprogress.js",
-      "./assets/js/Shortcode.js",
+      "./assets/js/Shortcode.js"
+    ])
+    .pipe(concat("plugins.min.js"))
+    .pipe(uglify())
+    .pipe(gulp.dest("./assets/dist"));
+  gulp
+    .src([
+      "./assets/js/url-polyfill.min.js",
+      "./assets/ghostHunter/dist/jquery.ghosthunter.js",
       "./assets/js/scripts.js"
     ])
     .pipe(uglify())
